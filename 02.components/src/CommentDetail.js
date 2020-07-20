af6420308b7918b6f5a59f9af15934f5fef53793
2NTH <hieuntctk42@gmail.com>
2020-07-20 21:42:13 +0700

    02.Props system - Pass value from parent to children

--- 02.components/src/CommentDetail.js
+++ 02.components/src/CommentDetail.js
@@ -1,20 +1,21 @@
 import React from "react";
-import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = props => {
+  const { author, timeAgo, text, avatar } = props;
+
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={faker.image.avatar()} alt="avatar" />
+        <img src={avatar} alt="avatar" />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{text}</div>
       </div>
     </div>
   );
--- 02.components/src/index.js
+++ 02.components/src/index.js
@@ -7,9 +7,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 2:00AM"
+        text="Nice blog post!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 4:00PM"
+        text="Have a nice day!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 5:00PM"
+        text="Great sharing!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
