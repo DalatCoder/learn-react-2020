bb091b0dc71c3b80328a7c77fc207de55a91bbab
2NTH <hieuntctk42@gmail.com>
2020-07-21 18:45:28 +0700

    03.Ternary operator

--- 03.seasons-class-component/src/SeasonDisplay.js
+++ 03.seasons-class-component/src/SeasonDisplay.js
@@ -1,12 +1,24 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = props => {
   const { lat } = props;
+  const month = new Date().getMonth();
+
+  const season = getSeason(lat, month);
+  const text =
+    season === "winter" ? "Burr, it is chilly!" : `Let's hit the beach!`;
 
   return (
     <div>
-      <h1>From season display component</h1>
-      <p>Latitue: {lat}</p>
+      <h1>{text}</h1>
     </div>
   );
 };
