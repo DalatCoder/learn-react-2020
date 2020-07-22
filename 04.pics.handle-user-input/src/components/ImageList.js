4539e4e396d842810a0058004f1480ef9e3464b2
2NTH <hieuntctk42@gmail.com>
2020-07-22 16:16:44 +0700

    04.Grid system

--- 04.pics.handle-user-input/src/components/ImageList.js
+++ 04.pics.handle-user-input/src/components/ImageList.js
@@ -1,14 +1,14 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = props => {
   const { images } = props;
-  console.log(images);
 
   const imageList = images.map(({ id, urls, alt_description }) => {
     return <img key={id} src={urls.regular} alt={alt_description} />;
   });
 
-  return <div>{imageList}</div>;
+  return <div className="image-list">{imageList}</div>;
 };
 
 export default ImageList;
