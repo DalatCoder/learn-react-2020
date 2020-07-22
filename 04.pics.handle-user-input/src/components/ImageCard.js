784ef2812cfa0f337a840d55a4141192fab53fc9
2NTH <hieuntctk42@gmail.com>
2020-07-22 17:56:54 +0700

    04.Complete v2

--- 04.pics.handle-user-input/src/components/ImageCard.js
+++ 04.pics.handle-user-input/src/components/ImageCard.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // Wait until the image successfully load up
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    // 10 is the default height of each image
+    const spans = Math.ceil(height / 10 + 1);
+
+    this.setState({ spans });
+  };
+
   render = () => {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   };
