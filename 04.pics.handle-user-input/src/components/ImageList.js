91076dfaca529a1fa760f30d4b92438573c6b51a
2NTH <hieuntctk42@gmail.com>
2020-07-22 15:44:43 +0700

    04.Render image list

--- 04.pics.handle-user-input/src/components/ImageList.js
+++ 04.pics.handle-user-input/src/components/ImageList.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const ImageList = props => {
   const { images } = props;
-
   console.log(images);
 
-  return <div>Hello image list</div>;
+  const imageList = images.map(image => {
+    return <img src={image.urls.regular} alt={image.alt_description} />;
+  });
+
+  return <div>{imageList}</div>;
 };
 
 export default ImageList;
