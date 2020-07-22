d7db3d8ff1192119bc0e575571832eee4de107e5
2NTH <hieuntctk42@gmail.com>
2020-07-22 15:32:21 +0700

    04.Add ImageList component

--- 04.pics.handle-user-input/src/components/App.js
+++ 04.pics.handle-user-input/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
@@ -10,7 +11,6 @@ class App extends React.Component {
       params: { query: value }
     });
 
-    console.log(data.results);
     this.setState({ images: data.results });
   };
 
@@ -18,7 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: '50px' }}>
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
-        Found {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
---
+++ 04.pics.handle-user-input/src/components/ImageList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageList = props => {
+  const { images } = props;
+
+  console.log(images);
+
+  return <div>Hello image list</div>;
+};
+
+export default ImageList;
