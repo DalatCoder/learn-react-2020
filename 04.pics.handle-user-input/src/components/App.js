6d0e7b1e9c850cfb96da63d02d057be85321d6df
2NTH <hieuntctk42@gmail.com>
2020-07-22 15:16:12 +0700

    04.Fetch data with axios

--- 04.pics.handle-user-input/src/components/App.js
+++ 04.pics.handle-user-input/src/components/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(value) {
-    console.log(`User submit form with value of: ${value}`);
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async value => {
+    const { data } = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: value
+      },
+      headers: {
+        Authorization: 'Client-ID mO_3krchfL-gTxVcORoxGenytpNH6ESsfcBZkOG4YGI'
+      }
+    });
+
+    console.log(data.results);
+    this.setState({ images: data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '50px' }}>
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images
       </div>
     );
   }
