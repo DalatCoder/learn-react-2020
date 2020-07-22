465011654937f3a533199c97b3f359e6bdfcbe3e
2NTH <hieuntctk42@gmail.com>
2020-07-22 15:58:23 +0700

    04.Complete v1

--- 04.pics.handle-user-input/src/components/ImageList.js
+++ 04.pics.handle-user-input/src/components/ImageList.js
@@ -4,8 +4,8 @@ const ImageList = props => {
   const { images } = props;
   console.log(images);
 
-  const imageList = images.map(image => {
-    return <img src={image.urls.regular} alt={image.alt_description} />;
+  const imageList = images.map(({ id, urls, alt_description }) => {
+    return <img key={id} src={urls.regular} alt={alt_description} />;
   });
 
   return <div>{imageList}</div>;
