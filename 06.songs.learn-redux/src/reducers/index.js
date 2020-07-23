1fb47da86bfd8284c9512e093f4487e9ef3a3916
2NTH <hieuntctk42@gmail.com>
2020-07-23 18:05:21 +0700

    06.Add reducers

---
+++ 06.songs.learn-redux/src/reducers/index.js
@@ -0,0 +1,27 @@
+import { combineReducers } from 'redux';
+
+// Static reducre, only return an array of songs
+// No need to provide any params
+const songsReducer = () => {
+  return [
+    { title: 'Duoi nhung con mua', length: '4:05' },
+    { title: 'Nang am xa dan', length: '3:05' },
+    { title: '1 phut', length: '6:05' },
+    { title: 'Thi thoi', length: '4:45' }
+  ];
+};
+
+// The first time selectedSongReducer gets invoked
+// The defalt value of selectedSong is null
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SELECTED_SONG') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
