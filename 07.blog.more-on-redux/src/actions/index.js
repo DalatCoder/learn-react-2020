a037ca849cd1d8325b6e5636ae227b21caa83091
2NTH <hieuntctk42@gmail.com>
2020-07-24 08:17:21 +0700

    07.Async creators

--- 07.blog.more-on-redux/src/actions/index.js
+++ 07.blog.more-on-redux/src/actions/index.js
@@ -1,5 +1,7 @@
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH_POSTS'
-  };
+import jsonPlacerholder from '../apis/jsonPlacerholder';
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlacerholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
