4c3131027b0c9f7f41974ef5636d6593d2dd4d14
2NTH <hieuntctk42@gmail.com>
2020-07-24 12:31:45 +0700

    07.Refactoring

--- 07.blog.more-on-redux/src/actions/index.js
+++ 07.blog.more-on-redux/src/actions/index.js
@@ -5,8 +5,16 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
   // Get an array of unique userId
+  /*
   const userIds = _.uniq(_.map(getState().posts, 'userId'));
   userIds.forEach(id => dispatch(fetchUser(id)));
+  */
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); // Execute
 };
 
 export const fetchPosts = () => async dispatch => {
