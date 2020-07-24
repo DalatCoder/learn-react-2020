d0648b5a44f999bd59e02eee933e9f5068b78d32
2NTH <hieuntctk42@gmail.com>
2020-07-24 21:22:24 +0700

    08.Check signIn status

--- 08.streams.react-router/client/src/components/GoogleAuth.js
+++ 08.streams.react-router/client/src/components/GoogleAuth.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '945254038998-57vduh000lc5eu5i9ab1t7a22tf211nr.apps.googleusercontent.com',
-        scope: 'email'
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '945254038998-57vduh000lc5eu5i9ab1t7a22tf211nr.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
+  renderAuthButton() {
+    if (!this.state.isSignedIn) {
+      return <div>Not signed in</div>;
+    }
+
+    return <div>Signed In</div>;
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
