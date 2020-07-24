e7725f06ee95b150c24506207d866cc7d853cba6
2NTH <hieuntctk42@gmail.com>
2020-07-24 21:40:38 +0700

    08.Complete OAuth2 with google

--- 08.streams.react-router/client/src/components/GoogleAuth.js
+++ 08.streams.react-router/client/src/components/GoogleAuth.js
@@ -14,16 +14,44 @@ class GoogleAuth extends React.Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  onAuthChange = () => {
+    // To make React rerender instantly when user signIn/SignOut
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
-    if (!this.state.isSignedIn) {
-      return <div>Not signed in</div>;
+    if (this.state.isSignedIn === null) {
+      return null;
     }
 
-    return <div>Signed In</div>;
+    if (this.state.isSignedIn === false) {
+      return (
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+
+    return (
+      <button className="ui red google button" onClick={this.onSignOut}>
+        <i className="google icon" />
+        Sign Out
+      </button>
+    );
   }
 
   render() {
