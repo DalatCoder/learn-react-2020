2bc2c0f2eb7d36918de396b811e568803658487b
2NTH <hieuntctk42@gmail.com>
2020-07-28 12:10:27 +0700

    08.Complete edit page

--- 08.streams.react-router/client/src/components/streams/StreamEdit.js
+++ 08.streams.react-router/client/src/components/streams/StreamEdit.js
@@ -1,16 +1,36 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
-    const streamId = this.props.match.params.id;
-    this.props.fetchStream(streamId);
+    this.streamId = this.props.match.params.id;
+    this.props.fetchStream(this.streamId);
   }
 
+  onFormSubmit = formValues => {
+    this.props.editStream(this.streamId, formValues);
+  };
+
   render() {
-    console.log(this.props.stream);
-    return <div>StreamEdit</div>;
+    const { stream } = this.props;
+
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+    // InitialValues is the special props for redux form to render initial
+    // value on inputs InitialValues ==> ReduxForm ==> StreamForm
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(stream, 'title', 'description')}
+          onSubmit={this.onFormSubmit}
+        />
+      </div>
+    );
   }
 }
 const mapStateToProps = (state, props) => {
@@ -20,4 +40,6 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
