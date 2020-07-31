f876a204ad241c86b21db7ded5a1a892d03d13af
2NTH <hieuntctk42@gmail.com>
2020-07-31 11:43:29 +0700

    09.Consumer for multiple context objects

--- 09.translate.context-system/src/components/Button.js
+++ 09.translate.context-system/src/components/Button.js
@@ -2,15 +2,17 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-  // Hooks up LanguageContext to Button component
-  static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Xác nhận';
+  }
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Xác nhận';
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>{this.renderSubmit}</LanguageContext.Consumer>
+      </button>
+    );
   }
 }
-
-// Button.contextType = LanguageContext;
 
 export default Button;
